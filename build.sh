06441062c89d6303d486b45c7d4e89fcdb543f59
savaadmuhammed007 <savaadmuhammed.psd@gmail.com>
2026-08-09 19:07:03 +0530

    Add runtime.txt and update build.sh for Render deployment

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 # exit on error
 set -o errexit
 
+pip install --upgrade pip
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py migrate
-
+python manage.py migrate --noinput || echo "Migration skipped during build"
