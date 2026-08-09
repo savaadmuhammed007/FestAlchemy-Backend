d6d82bbf6dc67d6269b0fc71e769ae1df666f3b5
savaadmuhammed007 <savaadmuhammed.psd@gmail.com>
2026-08-09 18:59:49 +0530

    Update app.py fallback entry point

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
+import os
 from festalchemy.wsgi import application as app
+
+if __name__ == '__main__':
+    port = os.environ.get('PORT', '8000')
+    from django.core.management import execute_from_command_line
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'festalchemy.settings')
+    execute_from_command_line(['manage.py', 'runserver', f'0.0.0.0:{port}'])
